511af8a00a27745ad61c5f1a341d162aa96e3441
viettt76 <viettt76@gmail.com>
2024-03-19 08:57:20 +0700

    complete page detail course (error last time)

--- src/store/reducers/configStore.js
+++ src/store/reducers/configStore.js
@@ -1,21 +1,21 @@
-import { createStore } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+// import { createStore } from 'redux';
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
 
-import rootReducer from './reducers';
+// import rootReducer from './rootReducer';
 
-const persistConfig = {
-    key: 'root',
-    storage,
-    whitelist: ['email', 'email_verified', 'family_name', 'given_name', 'locale', 'name', 'picture'],
-};
+// const persistConfig = {
+//     key: 'root',
+//     storage,
+//     whitelist: ['email', 'email_verified', 'family_name', 'given_name', 'locale', 'name', 'picture'],
+// };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const configStore = () => {
-    let store = createStore(persistedReducer);
-    let persistor = persistStore(store);
-    return { store, persistor };
-};
+// const configStore = () => {
+//     let store = createStore(persistedReducer);
+//     let persistor = persistStore(store);
+//     return { store, persistor };
+// };
 
-export default configStore;
+// export default configStore;
